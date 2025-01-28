56c41acdc8f050a88bf9597b530b0c1124703b67
anarcho <aaron.karlsen@gmail.com>
2025-01-28 10:32:56 +1000

    added desktop configuration

---
+++ configurations/nixos/desktop/default.nix
@@ -0,0 +1,38 @@
+{
+  flake,
+  config,
+  pkgs,
+  ...
+}: let
+  inherit (flake) inputs;
+  inherit (inputs) self;
+in {
+  imports = [
+    self.nixosModules.default
+    self.nixosModules.desktop
+    self.nixosModules.programs
+    ./configuration.nix
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  users.defaultUserShell = pkgs.zsh;
+
+  programs.zsh.enable = true;
+
+  desktop.modules.wm.bspwm = {
+    enable = true;
+    isVirtualMachine = true;
+    videoDrivers = ["virtualbox"];
+  };
+
+  programs.modules.notes.obsidian.enable = true;
+  programs.modules.music.spotify.enable = true;
+  programs.modules.shell.shell.enable = true;
+
+  home-manager.users."anarcho" = {
+    imports = [(self + /configurations/home/anarcho.nix)];
+  };
+
+  home-manager.backupFileExtension = "backup";
+}
