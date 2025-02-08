69beda295f6fe520ce4dcd0d48a304ebf228f509
aaronk <>
2025-02-08 15:40:50 +1000

    updating hyprpaper module

--- modules/home/nix/wm/hyprpaper.nix
+++ modules/home/nix/wm/hyprpaper.nix
@@ -23,9 +23,6 @@ in
       services.hyprpaper = {
         enable = true;
         settings = {
-          ipc = "on";
-          splash = false;
-          splash_offset = 2.0;
           reload = [cfg.wallPaper];
           wallpapers = [
             ",${cfg.wallPaper}"
